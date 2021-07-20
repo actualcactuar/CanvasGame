e56f221192b11305733f4c16144d35a7bd184b3e
jiri <kiviniemi.jiri@gmail.com>
2021-07-20 22:17:05 +0300

    animate explosion

--- modules/explosion.js
+++ modules/explosion.js
@@ -13,27 +13,54 @@ export default class Explosion extends GameObject {
    */
   constructor(game, size, x, y) {
     super(game, size, x, y, explosionImage);
-    this.targetSize = size;
-    this.size = 0.01;
+    this.scale = 0;
+    this.alpha = 1;
   }
 
   onUpdate() {
-    if (this.size < this.targetSize) {
-      this.size = this.size + 0.05;
+    switch (true) {
+      case this.scale < 2:
+        this.scale = this.scale + 0.05;
+        break;
+      case this.scale > 2 && this.alpha > 0:
+        const nextAlpha = this.alpha - 0.1;
+        this.alpha = nextAlpha > 0 ? nextAlpha : 0;
+        break;
+      default:
+        this.game.pop(this);
     }
   }
 
-  // draw() {
-  //   const context = this.game.context;
-  //   context.save(); // save context so only player is affected
-  //   context.translate(this.center.x, this.center.y); // translate image to correct position
-  //   context.drawImage(
-  //     this.image,
-  //     this.drawHeight, // draw self to own center
-  //     this.drawWidth,
-  //     this.image.width * this.size,
-  //     this.image.height * this.size
-  //   ); // draw player to top left corner, so transform value is correct
-  //   context.restore(); // restore other canvas components
-  // }
+  /**
+   *
+   * @param {number} xOffset
+   * @param {number} yOffset
+   */
+  drawExplosion(xOffset = 0, yOffset = 0, scaleDelay = 0) {
+    const context = this.game.context;
+    context.save(); // save context so only player is affected
+    context.translate(this.center.x + xOffset, this.center.y + yOffset); // translate image to correct position
+    context.scale(this.scale - scaleDelay, this.scale - scaleDelay);
+    context.globalAlpha = this.alpha;
+    context.drawImage(
+      this.image,
+      this.drawHeight, // draw self to own center
+      this.drawWidth,
+      this.image.width * this.size,
+      this.image.height * this.size
+    ); // draw player to top left corner, so transform value is correct
+    context.restore(); // restore other canvas components
+  }
+
+  draw() {
+    if (this.scale > 0.5) {
+      this.drawExplosion(5, 10, 0.5);
+    }
+
+    if (this.scale > 0.3) {
+      this.drawExplosion(-10, -10, 0.3);
+    }
+
+    this.drawExplosion();
+  }
 }
