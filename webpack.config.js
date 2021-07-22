e03fef64faa8a275b6adba8b9f2fb33487fb0290
jiri <kiviniemi.jiri@gmail.com>
2021-07-22 23:29:47 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
+        test: /\.(svg)$/i,
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
